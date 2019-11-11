043a8b622844f7a107d2bcfd0d51676871d2486c
lf- <lf-@users.noreply.github.com>
2019-11-10 22:42:57 -0800

    eliminate dupe comment

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,14 +6,6 @@
 
 // You can delete this file if you're not using it
 
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
-
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
