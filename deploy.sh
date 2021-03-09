a1bdad57ddb3ecbca5abc68c5c45c2efb6a89ed1
lf- <software@lfcode.ca>
2021-03-09 03:41:36 -0800

    A pile of code changes

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ ! -e .deploy_config ]]; then
+	cat >&2 <<-EOF
+	Please create a .deploy_config with content:
+	DEPLOY_HOST=[the host you rsync to]
+	DEPLOY_DIR=[the directory you rsync to]
+	# optional
+	EXTRA_OPTS=[extra rsync options you want to use]
+	EOF
+
+	exit 1
+fi
+
+source .deploy_config
+# trailing slash: copy contents of directory into destination
+rsync --verbose --human-readable --recursive --links --times --new-compress --delete-delay ${EXTRA_OPTS} public/ "${DEPLOY_HOST}:${DEPLOY_DIR}"
