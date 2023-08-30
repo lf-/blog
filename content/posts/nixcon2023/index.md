a1f48b60382a1f5537b6f93569599c29508093ac
Jade Lovelace <software@lfcode.ca>
2023-08-29 18:29:47 -0700

    check in initial slides stuff

---
+++ content/posts/nixcon2023/index.md
@@ -0,0 +1,9 @@
++++
+title = "NixCon 2023: Finding things in nixpkgs and NixOS source"
+path = "/nixcon2023"
+draft = true
+tags = ["nix", "nixcon"]
+date = "2023-09-09"
++++
+
+
---
+++ content/posts/nixcon2023/slides/main.js
@@ -0,0 +1,14 @@
+import Reveal from "reveal.js";
+import RevealMarkdown from "reveal.js/plugin/markdown";
+import RevealHighlight from "reveal.js/plugin/highlight";
+import RevealNotes from "reveal.js/plugin/notes";
+
+Reveal.initialize({
+    hash: true,
+
+    // Learn about plugins: https://revealjs.com/plugins/
+    plugins: [RevealMarkdown, RevealHighlight, RevealNotes],
+    transitionSpeed: "fast",
+    transition: "none",
+    slideNumber: true,
+});
